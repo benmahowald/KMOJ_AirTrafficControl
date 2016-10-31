a0996d23e14a1fda3261e05fec2930b41d0329fb
Ben Mahowald <ben.mahowald@gmail.com>
2016-10-31 16:52:18 -0500

    add/edit/create clients framework

--- public/scripts/controllers/dashController.js
+++ public/scripts/controllers/dashController.js
@@ -6,14 +6,15 @@ app.controller('dashController', ['$scope', '$http', function($scope, $http) {
     var clientToSend = {
       client: $scope.clientz,
       // client_id: $scope.client_id,
-      client_contact: $scope.client_contact,
+      contact: $scope.contact,
       phone: $scope.phone,
       cell: $scope.cell,
       fax: $scope.fax,
       email: $scope.email,
-      website: $scope.website,
+      webiste: $scope.website,
       street: $scope.street,
       city: $scope.city,
+      state: $scope.state,
       zip: $scope.zip
     };
     console.log('clientToSend -----------', clientToSend);
@@ -29,26 +30,42 @@ app.controller('dashController', ['$scope', '$http', function($scope, $http) {
         }); // end then function
   }; //end submitClient
 
-  // $scope.getClient = function () {
-  //   $http({
-  //     method: 'GET',
-  //     url: '/client/' + $scope.client_id,
-  //   }).then(function (response){
-  //         console.log('get manage account success:', response.data);
-  //         $scope.clientData = response.data;
-  //       }, function (error) {
-  //         console.log('error in get;', error);
-  //       }); // end then function
-  //   }; // end getClients
-  //
-  //   $scope.getClient();
-  //
-  //   $scope.updateClient = function(data) {
-	// 	  console.log( 'Data', data );
-  // 		$http({
-  // 			method: 'PUT',
-  // 			url: '/client/' + $scope.client_id,
-  // 			data: data
-  // 		}).then($scope.getClient);
+  $scope.getAllClients = function () {
+    $http({
+      method: 'GET',
+      url: '/client/' + $scope.client_id,
+    }).then(function (response){
+          $scope.allClients = response.data;
+          $scope.clientNames = response.data.name;
+          console.log('getAllClients success:', $scope.allClients);
+        }, function (error) {
+          console.log('error in getAllClients;', error);
+        }); // end then function
+    }; // end getAllClients
+
+  $scope.getAllClients();
+
+  $scope.getClient = function () {
+    $http({
+      method: 'GET',
+      url: '/client/' + $scope.client_id,
+    }).then(function (response){
+          console.log('get manage account success:', response.data);
+          $scope.clientData = response.data;
+        }, function (error) {
+          console.log('error in get;', error);
+        }); // end then function
+    }; // end getClients
+
+    $scope.getClient();
+
+    $scope.updateClient = function(data) {
+		  console.log( 'Data', data );
+  		$http({
+  			method: 'PUT',
+  			url: '/client/' + $scope.client_id,
+  			data: data
+  		}).then($scope.getClient);
+    }; // end updateClient
 
 }]); // end dashController
