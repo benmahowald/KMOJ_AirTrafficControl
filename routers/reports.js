92cb08ad357e098ba2d5b3190323360e6fee7801
Dave Kuntz <dkuntz811@gmail.com>
2016-11-01 13:05:11 -0500

    still learning not to work in master

--- routers/reports.js
+++ routers/reports.js
@@ -5,27 +5,27 @@ var pg = require('pg');
 var bodyParser = require('body-parser');
 var connectionString = 'postgres://localhost:5432/kmoj';
 
-// //generate a report from by getting info from db
-// router.get('/reports', function (req, res){
-// 	console.log('in get reports');
-// 	pg.connect(connectionString, function(err, client, done){
-// 		if (err){
-// 			console.log('connection err in reports');
-// 		} else {
-// 			var results = [];
-// 			var queryResults = client.query('SELECT flight.cart_number, flight.start_date, flight.end_date, clients.name, users.name, master.total_cost') +
-// 																		 ('FROM flight INNER JOIN clients ON clients.client_id = flight.id INNER JOIN users ON users.id = flight.id INNER JOIN master ON master.id = flight.id');
-//           queryResults.on('row', function(row){
-//             results.push(row);
-// 					});//end queryResults.on 'row'
-// 					queryResults.on('end', function(){
-// 						invoiceMail();
-// 						done();
-// 						return res.json(results);
-// 						console.log('results are', results);
-// 					});//end queryResults on 'end'
-// 		}
-// 	});//end pg.connect for getting reports
-// });//end router.get for getting reports
+//generate a report from by getting info from db
+router.get('/reports', function (req, res){
+	console.log('in get reports');
+	pg.connect(connectionString, function(err, client, done){
+		if (err){
+			console.log('connection err in reports');
+		} else {
+			var results = [];
+			var queryResults = client.query('SELECT flight.cart_number, flight.start_date, flight.end_date, clients.name, users.name, master.total_cost') +
+																		 ('FROM flight INNER JOIN clients ON clients.client_id = flight.id INNER JOIN users ON users.id = flight.id INNER JOIN master ON master.id = flight.id');
+          queryResults.on('row', function(row){
+            results.push(row);
+					});//end queryResults.on 'row'
+					queryResults.on('end', function(){
+						invoiceMail();
+						done();
+						return res.json(results);
+						console.log('results are', results);
+					});//end queryResults on 'end'
+		}
+	});//end pg.connect for getting reports
+});//end router.get for getting reports
 
 module.exports = router;
