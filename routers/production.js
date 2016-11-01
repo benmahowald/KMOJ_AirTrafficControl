158f01dff162503b9d63f634ebf598386836477b
Dave Kuntz <dkuntz811@gmail.com>
2016-11-01 10:25:24 -0500

    accidentally working on master

--- routers/production.js
+++ routers/production.js
@@ -1,39 +1,35 @@
-// var express = require('express');
-// var router = express.Router();
-// var path = require('path');
-//
-// router.get ('/production', function (req, res){
-// 	console.log ('=================');
-// });
-// // router.get for production view
-//
-// // console.log('in production router');
-// // router.post for production table
-// router.post ('/production', function (req, res){
-// 	console.log ('req.body for client is', req.body);
-// 	var client = req.body;
-// 	pg.connect(connectionString, function (err, client, done){
-// 		console.log('In production right now');
-// 		if (err){
-// 			console.log('connection error in client', client);
-// 		} else {
-// 			console.log('in the else now');
-// 			var clientInfo = [];
-//
-// 			client.query ('INSERT INTO production (contract_id, talent, who, what, site, why, cart_number, producer, complete_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);',
-// 			[req.body.contract_id, req.body.talent, req.body.who, req.body.what,
-// 			req.body.site, req.body.why, req.body.cart_number, req.body.producer, req.body.complete_date]);
-// 		}
-//
-//
-// 		queryResults.on('end', function(){
-// 			done();
-// 			// res.send({success: true});
-// 			return res.json(clientInfo);
-//
-// 		});//end queryResults for client table
-//
-// 	});//end pg.connect for client table
-// });//end router.post for client table
-//
-// module.exports = router;
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/kmoj';
+
+router.get ('/production', function (req, res){
+	console.log ('=================');
+});
+// router.get for production view
+
+// console.log('in production router');
+// router.post for production table
+router.post ('/production', function (req, res){
+	console.log ('req.body for client is', req.body);
+	var client = req.body;
+	pg.connect(connectionString, function (err, client, done){
+		console.log('In production right now');
+		if (err){
+			console.log('connection error in client', client);
+		} else {
+			console.log('in the else now');
+			var queryResults = client.query ('INSERT INTO production (contract_id, talent, who, what, site, why, cart_number, producer, complete_date, spot_length) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);',
+			[client.contract_id, client.talent, client.who, client.what,
+			client.site, client.why, client.cart_number, client.producer, client.complete_date, client.spot_length]);
+		}
+		queryResults.on('end', function(){
+			done();
+			res.send({success: true});
+		});//end queryResults for client table
+
+	});//end pg.connect for client table
+});//end router.post for client table
+
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -31,26 +31,26 @@ app.use('/admin', admin);
 var traffic = require ('../routers/traffic');
 app.use('/traffic', traffic);
 
-// var production = require ('../routers/production');
-// app.use('/production78', production);
+var production = require ('../routers/production');
+app.use('/production', production);
 
-app.post ('/production76', function (req, res){
-	console.log ('req.body for client is', req.body);
-	var client = req.body;
-	pg.connect(connectionString, function (err, client, done){
-		if (err){
-			console.log('connection error in client', client);
-		} else {
-			var queryResults = client.query ('INSERT INTO production (contract_id, talent, who, what, site, why, cart_number, producer, complete_date) '+
-																				'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)' ,
-																			[req.body.contract_id, req.body.talent, req.body.who, req.body.what, req.body.site, req.body.why, req.body.cart_number, req.body.producer, req.body.complete_date]);
-		}
-		queryResults.on('end', function(){
-			done();
-			res.send({success: true});
-		});//end queryResults for client table
-	});//end pg.connect for client table
-});//end router.post for client table
+// app.post ('/production76', function (req, res){
+// 	console.log ('req.body for client is', req.body);
+// 	var client = req.body;
+// 	pg.connect(connectionString, function (err, client, done){
+// 		if (err){
+// 			console.log('connection error in client', client);
+// 		} else {
+// 			var queryResults = client.query ('INSERT INTO production (contract_id, talent, who, what, site, why, cart_number, producer, complete_date) '+
+// 																				'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)' ,
+// 																			[req.body.contract_id, req.body.talent, req.body.who, req.body.what, req.body.site, req.body.why, req.body.cart_number, req.body.producer, req.body.complete_date]);
+// 		}
+// 		queryResults.on('end', function(){
+// 			done();
+// 			res.send({success: true});
+// 		});//end queryResults for client table
+// 	});//end pg.connect for client table
+// });//end router.post for client table
 
 
 var reports = require ('../routers/reports');
