5974f889b57fb9a85eed066ecb72198a75efa1be
Dave Kuntz <dkuntz811@gmail.com>
2016-11-01 13:12:47 -0500

    seriously still on master

--- public/scripts/controllers/reportController.js
+++ public/scripts/controllers/reportController.js
@@ -1,3 +1,17 @@
 app.controller('reportController', ['$scope', function($scope){
   console.log('Report Controller');
+
+  $scope.reports = [];
+
+  $scope.getReports = function (){
+    $http({
+      method: 'GET',
+      url: '/reports/reports'
+    }).then(function(response){
+      $scope.reports = response.data;
+    }, function errorCallback(response){
+      console.log('error getting reports', response);
+    }
+  });
+
 }]);
