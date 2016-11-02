ed57ce6b3245792190851becc1b59312b8ff7dcd
Johanna Snow <johannadsnow@gmail.com>
2016-11-02 18:05:18 -0500

    cleaned up code

--- public/scripts/controllers/prodController.js
+++ public/scripts/controllers/prodController.js
@@ -41,24 +41,7 @@ app.controller('prodController', ['$scope', '$http', function($scope, $http){
       }, function (error) {
         console.log('error in prodCtrl post route:', error);
       }); // end then function
-}; // end sendProduction
-//   var prodToSend = {
-//     talent: $scope.talent,
-//     who: $scope.who,
-//     what: $scope.what,
-//     site: $scope.site,
-//     why: $scope.why,
-//     cart_number: $scope.cart_number,
-//     producer: $scope.producer,
-//     spot_length: $scope.spot_length,
-//     complete_date: new Date()
-//   };
-//
-//
-// // send data onto server through post route
-// $scope.sendProduction = function (prodtoSend) {
-//   console.log('prodToSend', prodToSend);
-
+}; 
 
 
 }]);
