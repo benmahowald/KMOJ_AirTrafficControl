18c55afd4cb77271c0743041548e3d482c40b25f
Johanna Snow <johannadsnow@gmail.com>
2016-11-02 20:43:02 -0500

    removed 404 that was shoing on DOM but wasn't a true 404 just a numba

---
+++ public/views/routers/admin.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+var firebase = require('firebase');
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/kmoj';
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+router.use(bodyParser.json());
+
+// array to hold all `users`
+var user=[];
+
+router.post("/createNewUser", function(req, res){
+  firebase.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
+    console.log(decodedToken);
+    if(decodedToken.email == "test@test.com"){
+      console.log("Congrats " + decodedToken.email + "! You added " + req.body.email );
+      pg.connect(connectionString, function (err, client, done){
+        if (err){
+          console.log('error', error);
+        }//end if error
+        else {
+          var queryResults = client.query ('INSERT INTO users (email, permission, name, active) VALUES ($1, $2, $3, $4)', [req.body.email,req.body.permission,req.body.name,req.body.active]
+      );//end queryResults
+    }//end else
+        queryResults.on('end', function(){
+          done();
+          res.send({success: true});
+        });//end queryResults
+      });//end pg.connect
+    } //end if admin check
+    else {
+      res.send("You are not authorized to create a new user");
+    }//end else
+  }).catch(function(error) {
+    console.log(error);
+    res.send("Error",error);
+  });//end error
+});//end createNewUser
+
+//get list of users from server
+router.get('/userList', function(req,res){
+  console.log('in userList');
+  //pg connect
+  pg.connect( connectionString, function( err, client, done ){
+    //if err
+    if( err ){
+      console.log( 'error: ',err );
+    } //end if error connecting
+    else{
+      console.log( 'connected to database' );
+      //clear user array
+      user = [];
+      // query call to database table
+      var queryResults = client.query( 'SELECT * FROM users ORDER BY active ASC' );
+      queryResults.on( 'row', function( row ){
+        // runs for each row in the query result
+        user.push( row );
+      }); // end on row
+      queryResults.on( 'end', function(){
+        // we're done
+        done();
+        console.log('user: ',user);
+        // return result as a json version of array
+        return res.json( user );
+      });//end on end
+    }; // end no error
+  }); // end connect
+});//end userList
+
+//delete task
+router.delete('/deleteUser', function(req,res){
+  console.log('in deleteUser');
+  pg.connect(connectionString, function(err,client,done){
+    if(err){
+      console.log('error: ',err);
+    }//end if
+    else{
+      console.log('connected to database in deleteUser');
+      console.log('Deleted: ', req.body);
+      //clear resultArray
+      var resultArray = [];
+      var resultQuery=client.query('DELETE FROM users WHERE id=($1)',[req.body.id]);
+      resultQuery.on('row', function(row){
+        resultArray.push(row);
+      });//end on row
+      resultQuery.on('end', function(){
+        done();
+        return res.json(resultArray);
+      });//end on end
+    };//end else
+  });//end pg connect
+});//end deleteUser
+
+module.exports = router;
---
+++ public/views/routers/traffic.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var pg = require('pg');
+var bodyParser = require('body-parser');
+var nodemailer = require ('nodemailer');
+var connectionString = 'postgres://localhost:5432/kmoj';
+
+// console.log('in traffic router');
+//router.post slots
+router.post ('/slots', function (req, res){
+	console.log ('req.body for slots is', req.body);
+	var slots = req.body;
+	pg.connect(connectionString, function (err, client, done){
+		if (err){
+			console.log('connection error in slots', slots);
+		} else {
+			var queryResults = client.query ('INSERT INTO slots (day_of_run, plays, slot, flight_id,) '+
+																				'VALUES ($1, $2, $3, $4)' ,
+																			[req.body.day_of_run, req.body.plays, req.body.slot, req.body.flight_id]);
+		}
+		queryResults.on('end', function(){
+			done();
+			res.send({success: true});
+		});//end queryResults for slots table
+	});//end pg.connect for slots table
+});//end router.post for slots table
+
+
+// console.log('in traffic router');
+//router.post media (interviews, socialmedia)
+router.post ('/media', function (req, res){
+	console.log ('req.body for media is', req.body);
+	var media = req.body;
+	pg.connect(connectionString, function (err, client, done){
+		if (err){
+			console.log('connection error in media', media);
+		} else {
+			var queryResults = client.query ('INSERT INTO master (interviews, socialmedia, man_app, uw_app, pr_app, tr_app) '+
+																				'VALUES ($1, $2, $3, $4, $5, $6) WHERE users_id = ($1)' ,
+																			[req.body.interviews, req.body.socialmedia, req.body.man_app, req.body.uw_app, req.body.pr_app, req.body.tr_app]);
+		}
+		queryResults.on('end', function(){
+			done();
+			res.send({success: true});
+
+		});//end queryResults for media table
+	});//end pg.connect for media table
+});//end router.post for media table
+
+
+//router.post flights
+
+router.post ('/flight', function (req, res){
+	console.log ('req.body for flight is', req.body);
+	var flight = req.body;
+	pg.connect(connectionString, function (err, flight, done){
+		if (err){
+			console.log('connection error in flight', flight);
+		} else {
+			var queryResults = flight.query ('INSERT INTO flight (contract_id, start_date, end_date, cart_number,) '+
+																				'VALUES ($1, $2, $3, $4)' ,
+																			[flight]);
+		}
+		queryResults.on('end', function(){
+			done();
+			res.send({success: true});
+		});//end queryResults for flight table
+	});//end pg.connect for flight table
+});//end router.post for flight table
+
+//get traffic info from db for traffic
+router.get('/trafficinfo', function (req, res){
+	console.log('in get traffic info');
+	pg.connect(connectionString, function(err, client, done){
+		if (err){
+			console.log('connection err in trafficinfo');
+		} else {
+			var results = [];
+			var queryResults = client.query('SELECT master.event_name, master.total_cost, master.sign_date, master.interviews, master.socialmedia, master.instructions, master.spot_type, master.spot_length, master.spot_rate, master.total_spots, users.name FROM master ' +
+																			'INNER JOIN users ON users.id = master.users_id;');
+					queryResults.on('row', function(row){
+						results.push(row);
+					});//end queryResults.on 'row'
+					queryResults.on('end', function(){
+						done();
+						return res.json(results);
+						console.log('results are', results);
+					});//end queryResults on 'end'
+		}
+	});//end pg.connect for traffic info
+});//end router.get for traffic info
+
+//get client info from db for traffic
+
+router.get('/clientinfo', function (req, res){
+	console.log('in get client info');
+	pg.connect(connectionString, function(err, client, done){
+		if (err){
+			console.log('connection err in clientinfo');
+		} else {
+			var results = [];
+			var queryResults = client.query('SELECT * FROM clients');
+					queryResults.on('row', function(row){
+						results.push(row);
+					});//end queryResults.on 'row'
+					queryResults.on('end', function(){
+						done();
+						return res.json(results);
+						console.log('results are', results);
+					});//end queryResults on 'end'
+		}
+	});//end pg.connect for traffic info
+});//end router.get for traffic info
+
+//get slots info
+router.get('/getslots', function (req, res){
+	console.log('in get slots');
+	pg.connect(connectionString, function(err, client, done){
+		if(err){
+			console.log('get slots connection error is', err);
+		} else {
+			var results = [];
+			var queryResults = client.query ('SELECT * FROM slots');
+			queryResults.on('row', function(row){
+				results.push(row);
+			});
+			queryResults.on('end', function(){
+				done();
+				return res.json(results);
+				console.log('slots results are', results);
+			});//end query results for getslots
+		}
+	}); //end pg.connect for getslots
+});//end router.getslots
+
+//get the flight info
+
+router.get('/getflight', function (req, res){
+	console.log('in get flight');
+	pg.connect(connectionString, function(err, client, done){
+		if(err){
+			console.log('get flight connection error is', err);
+		} else {
+			var results = [];
+			var queryResults = client.query ('SELECT * FROM flight');
+			queryResults.on('row', function(row){
+				results.push(row);
+			});
+			queryResults.on('end', function(){
+				done();
+				return res.json(results);
+				console.log('flight results are', results);
+			});//end queryResults for getflight
+		}
+	}); //end pg.connect for get flight
+});//end router.getflight
+
+
+
+
+module.exports = router;
