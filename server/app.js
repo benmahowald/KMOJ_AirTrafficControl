0419fd6380d7cafc86698c49d09375c3ca222955
Dave Kuntz <dkuntz811@gmail.com>
2016-11-03 16:45:27 -0500

    fixing reportController

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.use('/nodemailer', nodemailer);
 
 
 //port 5000 being used
-var portDecision = process.env.PORT || 6060;
+var portDecision = process.env.PORT || 8080;
 
 app.listen(portDecision, function(){
   console.log("Listening on port: ", portDecision);
