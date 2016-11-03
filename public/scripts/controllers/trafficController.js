9f69c3d084ebef03be58ad417ceb315c2cb39611
Ben Mahowald <ben.mahowald@gmail.com>
2016-11-03 16:17:22 -0500

    pending contract flight times showing

--- public/scripts/controllers/trafficController.js
+++ public/scripts/controllers/trafficController.js
@@ -7,9 +7,8 @@ app.controller('trafficController', ['$scope','$http', function($scope, $http){
       method: 'GET',
       url: '/traffic/contractsPending',
     }).then(function(response){
-      console.log('traffic contract success', response);
       $scope.pendingContracts = response.data;
-      console.log(response.data);
+      console.log('$scope.pendingContracts', $scope.pendingContracts);
     }, function errorCallback (response){
       console.log('err', response);
     }); // end then
@@ -26,30 +25,35 @@ app.controller('trafficController', ['$scope','$http', function($scope, $http){
     }).then(function(response){
       console.log('traffic contract success', response);
       $scope.flightInfo = response.data;
-      console.log(response.data);
+      console.log('$scope.flightInfo = ', $scope.flightInfo);
+      console.log('$scope.flightInfo.end_date = ', $scope.flightInfo[0].end_date);
+
+      $scope.start_date = moment($scope.flightInfo[0].start_date).format('ddd, MMM DD YYYY');
+      $scope.end_date = moment($scope.flightInfo[0].end_date).format('ddd, MMM DD YYYY');
+      console.log(response);
     }, function errorCallback (response){
       console.log('err', response);
     }); // end then
   }; // end selectContract
 
-  $scope.sendTraffic = function (){
-    var traffictoSend = {
-      interviews: $scope.interviews,
-      socialmedia: $scope.socialmedia,
-      man_app: $scope.man_app,
-      uw_app: $scope.uw_app,
-      pr_app: $scope.pr_app,
-      tr_app: $scope.pr_app
-    }; // end sendTraffic object
-
-    $http({
-      method: 'POST',
-      url: '/traffic/media',
-      data: traffictoSend
-    }).then(function(response){
-      console.log('traffic to send id', response);
-    }, function errorCallback (response){
-      console.log('err', response);
-    }); // end then
-  }; // end send traffic
+  // $scope.sendTraffic = function (){
+  //   var traffictoSend = {
+  //     interviews: $scope.interviews,
+  //     socialmedia: $scope.socialmedia,
+  //     man_app: $scope.man_app,
+  //     uw_app: $scope.uw_app,
+  //     pr_app: $scope.pr_app,
+  //     tr_app: $scope.pr_app
+  //   }; // end sendTraffic object
+  //
+  //   $http({
+  //     method: 'POST',
+  //     url: '/traffic/media',
+  //     data: traffictoSend
+  //   }).then(function(response){
+  //     console.log('traffic to send id', response);
+  //   }, function errorCallback (response){
+  //     console.log('err', response);
+  //   }); // end then
+  // }; // end send traffic
 }]); // end trafficController
