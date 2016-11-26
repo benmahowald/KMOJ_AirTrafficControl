fb74e290d8dfdba3e56cdbf35f751ae4a3596d62
Ben Mahowald <ben.mahowald@gmail.com>
2016-11-25 23:51:59 -0600

    search field checkin for NaN or String

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -1,3 +1,14 @@
 app.controller('searchController', ['$scope', function($scope){
   console.log('Search Controller');
-}]);
+
+  $scope.searchFunction = function () {
+    console.log('in searchFunction');
+    console.log(typeof(isNaN($scope.search)));
+    if(isNaN($scope.search) === true) {
+      console.log('search is NaN');
+    } else {
+      console.log('log IS a number');
+    }
+  }; // end searchFunction
+
+}]); // end searchController
