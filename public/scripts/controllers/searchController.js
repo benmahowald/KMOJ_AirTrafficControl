91099b780b48f9acbb41e58b63a05477b8ebd64e
Ben Mahowald <ben.mahowald@gmail.com>
2016-11-26 00:21:52 -0600

    structuring the search function

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -5,10 +5,27 @@ app.controller('searchController', ['$scope', function($scope){
     console.log('in searchFunction');
     console.log(typeof(isNaN($scope.search)));
     if(isNaN($scope.search) === true) {
-      console.log('search is NaN');
+      console.log('search is NOT a number');
+      $http({
+        method: 'GET',
+        url: '/production/production?q=' + $scope.currentProdId,
+      }).then(function (response){
+            console.log('success in prodCtrl post route:', response);
+            $scope.productionSaved = true;
+            $scope.clearFields();
+          }, function (error) {
+            console.log('error in prodCtrl post route:', error);
+          }); // end then function
+    }; // end http call
+      clearFields();
     } else {
       console.log('log IS a number');
+      clearFields();
     }
   }; // end searchFunction
+
+var clearFields = function () {
+  $scope.search = null;
+};
 
 }]); // end searchController
