b025c142457261846c77b0a35aa9a7240b16dfc0
Dave Kuntz <dkuntz811@gmail.com>
2016-12-02 16:31:45 -0600

    small traffic change

--- public/scripts/controllers/trafficController.js
+++ public/scripts/controllers/trafficController.js
@@ -127,7 +127,7 @@ app.controller('trafficController', ['$scope','$http', function($scope, $http){
       }, //end table
         ['\n'],
           {text: 'Scheduled Station: KJOM', style: 'subheader', alignment: 'center'},
-          {text: '' + invoiceInfo.client_name + '\n\n\n', alignment: 'center'},
+          {text: '' + invoiceInfo.clients_name + '\n\n\n', alignment: 'center'},
 
       {
         table: { alignment: 'center',
