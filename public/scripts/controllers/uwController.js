d7072ce729bdd6f37fc83feb59b6c4f14de812a5
Luis H. Matos <ReachLuis@Gmail.com>
2016-12-04 23:28:51 -0600

    Comment and Refactor uwController for maintainability

--- public/scripts/controllers/uwController.js
+++ public/scripts/controllers/uwController.js
@@ -1,13 +1,13 @@
 app.controller('uwController', ['$scope', '$mdDialog', '$http',  function($scope, $mdDialog, $http){
   console.log('Underwriter Controller');
-  //// code for the traffic entry grid
-  // Initialize variables
+  //---- INITIALIZE VARIABLES
   // weeks will hold the actual scheduling information
   $scope.weeks = {week1:{num: 1}};
+  // totals will keep track of the totals for each week
   $scope.totals = {week1:{total: 0}};
-  $scope.currentNumWeeks = 1;
+  $scope.currentNumWeeks = 1; // current number of weeks be in rotation
   $scope.contractSaved = false;
-  // This object is used as a scaffold help build the grid
+  // This object is used to help scaffold the grid, and name properties in object
   $scope.hours = {
     am2: {fullText:'2a-5a', title:'am2'},
     am5: {fullText:'5a-6a', title:'am5'},
@@ -19,12 +19,15 @@ app.controller('uwController', ['$scope', '$mdDialog', '$http',  function($scope
     pm7: {fullText:'7p-10p', title:'pm7'},
     pm10: {fullText:'10p-2a', title:'pm10'}
   };
-  // This is used to populate the header and scaffold the grid
+  // This is used to populate the header, scaffold the grid, and name properties in object
   $scope.days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  // console.log($scope.hours);
 
+  $scope.clientNameList = []; // initialize array
+
+
+  //---- DECLARE FUNCTIONS
   $scope.buildFlight = function(){
-    // console.log($scope.weeks);
+    console.log('In buildFlight');
 
     var numDays = $scope.currentNumWeeks * 7; // seven days in a weeks
     var dayIndex;
@@ -56,103 +59,15 @@ app.controller('uwController', ['$scope', '$mdDialog', '$http',  function($scope
     console.log('slotDBinfo:', $scope.slotDBinfo);
   }; // end buildFlight()
 
-  $scope.updateWeeks = function(weekRequest, ev){
-    console.log('in updateWeeks with:', weekRequest);
-
-    var currentMaxWeek = $scope.currentNumWeeks;
-    // Disallow negative or 0 week requests
-    if (weekRequest <= 0) {
-      weekRequest = currentMaxWeek;
+  $scope.calcFlightTotal = function(){
+    console.log($scope.currentNumWeeks);
+    $scope.flightTotal = 0;
+    for (var l = 1; l <= $scope.currentNumWeeks; l++) {
+      $scope.flightTotal = $scope.flightTotal + $scope.totals['week'+l].total;
+      console.log('flightTotal:', $scope.flightTotal);
     }
-    console.log('currentMaxWeek:', currentMaxWeek);
-    var weeksDiff = weekRequest - currentMaxWeek;
-    console.log('weeksDiff:', weeksDiff);
-    if (weeksDiff > 0){
-      var nextWeekNum = currentMaxWeek+1;
-      var newTotalWeeks = currentMaxWeek+weeksDiff;
-      var weekToAdd;
-      for (var i = nextWeekNum; i <= newTotalWeeks; i++) {
-        weekToAdd = 'week'+i;
-        $scope.weeks[weekToAdd]={num: i};
-        $scope.totals[weekToAdd]={total: 0};
-        $scope.currentNumWeeks++;
-      } // end for loop
-    } else if (weeksDiff < 0) {
-      // make weeksDiff positive to be usable in various ways
-      weeksDiff = -weeksDiff;
-      var newMaxWeek = currentMaxWeek-weeksDiff;
-      var emptyOfInfo = true;
-      var searchDone = false;
-      var removeStart = newMaxWeek+1;
-      var weekInQuestion = removeStart;
-      var thisWeek;
-      // Check to see if any information has been entered in the weeks that are
-      // about to be deleted
-      while (emptyOfInfo && !searchDone) {
-        thisWeek = 'week'+weekInQuestion;
-        // check for any hours in the week object
-        for (var hour in $scope.weeks[thisWeek]) {
-          if ($scope.weeks[thisWeek].hasOwnProperty(hour)) {
-            // check for any days in the hour object
-            for (var day in $scope.weeks[thisWeek][hour]) {
-              if ($scope.weeks[thisWeek][hour].hasOwnProperty(day)) {
-                // if the value there is not equal to zero
-                if($scope.weeks[thisWeek][hour][day] !== 0) {
-                  // then we have info to lose!
-                  emptyOfInfo = false;
-                } // end zero check
-              }
-            } // end loop through days
-          }
-        } // end loop through hours
-        weekInQuestion++;
-        if (weekInQuestion>currentMaxWeek) {
-          searchDone = true;
-        }
-      }
-      // if the weeks in question are not empty of information
-      if (!emptyOfInfo) {
-        // then confirm the action with the user
-        var pluralText = 's';
-
-        if(weeksDiff===1){
-          pluralText = '';
-        }
-
-        var confirm = $mdDialog.confirm()
-        .title('Remove '+weeksDiff+' week'+pluralText+' - Are you sure?')
-        .textContent('You have entered information in the '+weeksDiff+' week'+pluralText+' you are about to remove.')
-        .ariaLabel('Removal Warning')
-        .targetEvent(ev)
-        .ok('Remove extra week'+pluralText)
-        .cancel('Cancel and review');
-
-        $mdDialog.show(confirm).then(function() {
-          console.log('You chose True!');
-          $scope.trimWeeks(removeStart, currentMaxWeek, thisWeek, emptyOfInfo);
-        }, function() {
-          console.log('You chose False!');
-        });
-      } else {
-        // else the week are empty of information and we can remove them
-        $scope.trimWeeks(removeStart, currentMaxWeek, thisWeek, emptyOfInfo);
-      }
-    } // end weeksDiff check (if-else)
-  };
-
-  $scope.trimWeeks = function(removeStart, currentMaxWeek, thisWeek, emptyOfInfo){
-    for (var j = removeStart; j <= currentMaxWeek; j++) {
-      thisWeek = 'week'+j;
-      delete $scope.weeks[thisWeek];
-      delete $scope.totals[thisWeek];
-      $scope.currentNumWeeks--;
-    }
-    // if it was not empty of info then we need to recalculate the totals
-    if (!emptyOfInfo){
-      //// Update the flight's total
-      $scope.calcFlightTotal();
-    }
-  };
+    console.log('totals:',$scope.totals);
+  }; // end calcFlightTotal
 
   $scope.checkInput = function(thisWeek, thisHour, thisDay){
     console.log('in checkInput, with:', thisDay, thisHour, thisWeek);
@@ -165,50 +80,66 @@ app.controller('uwController', ['$scope', '$mdDialog', '$http',  function($scope
     }
 
     $scope.updateTotals(thisWeek, thisHour, thisDay);
-  };
+  }; // end checkInput
 
-  $scope.updateTotals = function(thisWeek, thisHour, thisDay){
-    console.log('in updateTotals, with:', thisDay, thisHour, thisWeek);
-    var weekName = 'week'+thisWeek;
-    // Reset counters
-    $scope.totals[weekName][thisHour] = 0;
-    $scope.totals[weekName].total = 0;
-    var dayCheck;
-    // check to see if anything has been recorded yet
-    for (var k = 0; k < $scope.days.length; k++) {
-      dayCheck = $scope.days[k];
-      //// Update the hour's total
-      // if there is a total for that day then add it to the sum
-      if ($scope.weeks[weekName][thisHour][dayCheck]) {
-        $scope.totals[weekName][thisHour] =
-        $scope.totals[weekName][thisHour] +
-        $scope.weeks[weekName][thisHour][dayCheck];
+  $scope.clearFields = function () {
+    console.log('in clearFields');
+    $scope.event_name = null;
+    $scope.startDate = null;
+    $scope.endDate = null;
+    $scope.fa = null;
+    $scope.psa = null;
+    $scope.instructions = null;
+    $scope.discount = null;
+    $scope.agency_commission = null;
+    $scope.slotDBinfo = null;
+    $scope.totalCost = null;
+    $scope.numInterviews = null;
+    $scope.numSocialMedia = null;
+    $scope.spot_rate = null;
+    $scope.selectedName = null;
+    $scope.talent = null;
+    $scope.who = null;
+    $scope.what = null;
+    $scope.site = null;
+    $scope.why = null;
+    $scope.producer = null;
+    $scope.spot_length = null;
+    $scope.event_name = null;
+  }; // end clearFields
+
+  $scope.getAllClients = function () {
+    // retrieves all clients and pushs each name into clientNameList array
+    // for client drop down meny
+    console.log('in getAllClients');
+    $http({
+      method: 'GET',
+      url: '/clients',
+    }).then(function (response){
+      $scope.allClients = response.data;
+      // console.log('getAllClients success:', $scope.allClients);
+      for (var i = 0; i < $scope.allClients.length; i++) {
+        $scope.clientNameList.push($scope.allClients[i].name);
       }
-      //// Update the week's total
-      for (var hour in $scope.hours) {
-        if ($scope.hours.hasOwnProperty(hour)) {
-          if ($scope.weeks[weekName][hour] && $scope.weeks[weekName][hour][dayCheck]) {
-            $scope.totals[weekName].total =
-            $scope.totals[weekName].total +
-            $scope.weeks[weekName][hour][dayCheck];
-          }
-        }
-      }  // End for loop throuh hours in day
-    } // End for loop through days of week
+    }, function (error) {
+      console.log('error in getAllClients;', error);
+    }); // end then function
+  }; // end getAllClients
 
-    //// Update the flight's total
-    $scope.calcFlightTotal();
-  }; // end updateTotals
-
-  $scope.calcFlightTotal = function(){
-    console.log($scope.currentNumWeeks);
-    $scope.flightTotal = 0;
-    for (var l = 1; l <= $scope.currentNumWeeks; l++) {
-      $scope.flightTotal = $scope.flightTotal + $scope.totals['week'+l].total;
-      console.log('flightTotal:', $scope.flightTotal);
-    }
-    console.log('totals:',$scope.totals);
-  };
+  $scope.getClient = function () {
+    // calls for xeditable functionality to edit client info
+    console.log('in getClient');
+    console.log('selectedName:', $scope.selectedName);
+    $http({
+      method: 'GET',
+      url: '/client?q=' + $scope.selectedName,
+    }).then(function (response){
+      $scope.clientData = response.data;
+      console.log('$scope.clientData = ', $scope.clientData);
+    }, function (error) {
+      console.log('error in get;', error);
+    }); // end then function
+  }; // end getClient
 
   $scope.incrementCount = function(thisWeek, thisHour, thisDay, $event) {
     console.log('in incrementCount, with:', thisDay, thisHour, thisWeek, $event.target);
@@ -223,7 +154,7 @@ app.controller('uwController', ['$scope', '$mdDialog', '$http',  function($scope
     }
 
     $scope.updateTotals(thisWeek, thisHour, thisDay);
-  };
+  }; // end incrementCount
 
   $scope.submitRunSheetEntry = function (ev){
     console.log('in submitRunSheetEntry');
@@ -391,67 +322,139 @@ app.controller('uwController', ['$scope', '$mdDialog', '$http',  function($scope
         }//end else
   }; // end submitRunSheetEntry
 
-  $scope.clientNameList = [];
+  $scope.trimWeeks = function(removeStart, currentMaxWeek, thisWeek, emptyOfInfo){
+    for (var j = removeStart; j <= currentMaxWeek; j++) {
+      thisWeek = 'week'+j;
+      delete $scope.weeks[thisWeek];
+      delete $scope.totals[thisWeek];
+      $scope.currentNumWeeks--;
+    }
+    // if it was not empty of info then we need to recalculate the totals
+    if (!emptyOfInfo){
+      //// Update the flight's total
+      $scope.calcFlightTotal();
+    }
+  }; // end trimWeeks
 
-  // retrieves all clients and pushs each name into clientNameList array
-  // for client drop down meny
-  $scope.getAllClients = function () {
-    console.log('in getAllClients');
-    $http({
-      method: 'GET',
-      url: '/clients',
-    }).then(function (response){
-      $scope.allClients = response.data;
-      // console.log('getAllClients success:', $scope.allClients);
-      for (var i = 0; i < $scope.allClients.length; i++) {
-        $scope.clientNameList.push($scope.allClients[i].name);
+  $scope.updateTotals = function(thisWeek, thisHour, thisDay){
+    console.log('in updateTotals, with:', thisDay, thisHour, thisWeek);
+    var weekName = 'week'+thisWeek;
+    // Reset counters
+    $scope.totals[weekName][thisHour] = 0;
+    $scope.totals[weekName].total = 0;
+    var dayCheck;
+    // check to see if anything has been recorded yet
+    for (var k = 0; k < $scope.days.length; k++) {
+      dayCheck = $scope.days[k];
+      //// Update the hour's total
+      // if there is a total for that day then add it to the sum
+      if ($scope.weeks[weekName][thisHour][dayCheck]) {
+        $scope.totals[weekName][thisHour] =
+        $scope.totals[weekName][thisHour] +
+        $scope.weeks[weekName][thisHour][dayCheck];
       }
-    }, function (error) {
-      console.log('error in getAllClients;', error);
-    }); // end then function
-  }; // end getAllClients
+      //// Update the week's total
+      for (var hour in $scope.hours) {
+        if ($scope.hours.hasOwnProperty(hour)) {
+          if ($scope.weeks[weekName][hour] && $scope.weeks[weekName][hour][dayCheck]) {
+            $scope.totals[weekName].total =
+            $scope.totals[weekName].total +
+            $scope.weeks[weekName][hour][dayCheck];
+          }
+        }
+      }  // End for loop throuh hours in day
+    } // End for loop through days of week
 
+    //// Update the flight's total
+    $scope.calcFlightTotal();
+  }; // end updateTotals
+
+  $scope.updateWeeks = function(weekRequest, ev){
+    console.log('in updateWeeks with:', weekRequest);
+
+    var currentMaxWeek = $scope.currentNumWeeks;
+    // Disallow negative or 0 week requests
+    if (weekRequest <= 0) {
+      weekRequest = currentMaxWeek;
+    }
+    console.log('currentMaxWeek:', currentMaxWeek);
+    var weeksDiff = weekRequest - currentMaxWeek;
+    console.log('weeksDiff:', weeksDiff);
+    if (weeksDiff > 0){
+      var nextWeekNum = currentMaxWeek+1;
+      var newTotalWeeks = currentMaxWeek+weeksDiff;
+      var weekToAdd;
+      for (var i = nextWeekNum; i <= newTotalWeeks; i++) {
+        weekToAdd = 'week'+i;
+        $scope.weeks[weekToAdd]={num: i};
+        $scope.totals[weekToAdd]={total: 0};
+        $scope.currentNumWeeks++;
+      } // end for loop
+    } else if (weeksDiff < 0) {
+      // make weeksDiff positive to be usable in various ways
+      weeksDiff = -weeksDiff;
+      var newMaxWeek = currentMaxWeek-weeksDiff;
+      var emptyOfInfo = true;
+      var searchDone = false;
+      var removeStart = newMaxWeek+1;
+      var weekInQuestion = removeStart;
+      var thisWeek;
+      // Check to see if any information has been entered in the weeks that are
+      // about to be deleted
+      while (emptyOfInfo && !searchDone) {
+        thisWeek = 'week'+weekInQuestion;
+        // check for any hours in the week object
+        for (var hour in $scope.weeks[thisWeek]) {
+          if ($scope.weeks[thisWeek].hasOwnProperty(hour)) {
+            // check for any days in the hour object
+            for (var day in $scope.weeks[thisWeek][hour]) {
+              if ($scope.weeks[thisWeek][hour].hasOwnProperty(day)) {
+                // if the value there is not equal to zero
+                if($scope.weeks[thisWeek][hour][day] !== 0) {
+                  // then we have info to lose!
+                  emptyOfInfo = false;
+                } // end zero check
+              }
+            } // end loop through days
+          }
+        } // end loop through hours
+        weekInQuestion++;
+        if (weekInQuestion>currentMaxWeek) {
+          searchDone = true;
+        }
+      }
+      // if the weeks in question are not empty of information
+      if (!emptyOfInfo) {
+        // then confirm the action with the user
+        var pluralText = 's';
+
+        if(weeksDiff===1){
+          pluralText = '';
+        }
+
+        var confirm = $mdDialog.confirm()
+        .title('Remove '+weeksDiff+' week'+pluralText+' - Are you sure?')
+        .textContent('You have entered information in the '+weeksDiff+' week'+pluralText+' you are about to remove.')
+        .ariaLabel('Removal Warning')
+        .targetEvent(ev)
+        .ok('Remove extra week'+pluralText)
+        .cancel('Cancel and review');
+
+        $mdDialog.show(confirm).then(function() {
+          console.log('You chose True!');
+          $scope.trimWeeks(removeStart, currentMaxWeek, thisWeek, emptyOfInfo);
+        }, function() {
+          console.log('You chose False!');
+        });
+      } else {
+        // else the week are empty of information and we can remove them
+        $scope.trimWeeks(removeStart, currentMaxWeek, thisWeek, emptyOfInfo);
+      }
+    } // end weeksDiff check (if-else)
+  }; // end updateWeeks
+
+
+  // ---- RUN ON PAGE LOAD
   $scope.getAllClients();
-
-  // calls for xeditable functionality to edit client info
-  $scope.getClient = function () {
-    console.log('in getClient');
-    console.log('selectedName:', $scope.selectedName);
-    $http({
-      method: 'GET',
-      url: '/client?q=' + $scope.selectedName,
-    }).then(function (response){
-      $scope.clientData = response.data;
-      console.log('$scope.clientData = ', $scope.clientData);
-    }, function (error) {
-      console.log('error in get;', error);
-    }); // end then function
-  }; // end getClients
-
-  $scope.clearFields = function () {
-    // console.log('in clearFields');
-    $scope.event_name = null;
-    $scope.startDate = null;
-    $scope.endDate = null;
-    $scope.fa = null;
-    $scope.psa = null;
-    $scope.instructions = null;
-    $scope.discount = null;
-    $scope.agency_commission = null;
-    $scope.slotDBinfo = null;
-    $scope.totalCost = null;
-    $scope.numInterviews = null;
-    $scope.numSocialMedia = null;
-    $scope.spot_rate = null;
-    $scope.selectedName = null;
-    $scope.talent = null;
-    $scope.who = null;
-    $scope.what = null;
-    $scope.site = null;
-    $scope.why = null;
-    $scope.producer = null;
-    $scope.spot_length = null;
-    $scope.event_name = null;
-  }; // end clearFields
 
 }]); // end uwController
